33a61a85e9ab66f9d9c3a78c9bd8e81210e639af
Shaisiva <32162906+Shaisiva@users.noreply.github.com>
2026-01-21 15:56:58 +0530

    Update rotateArray.java

--- rotateArray.java
+++ rotateArray.java
@@ -1,20 +1,33 @@
-class Solution {
-    public void rotate(int[] nums, int k) {
-        
-        int n=nums.length-1;
-        reverse(nums,0,n);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n);
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;  // handle k > n
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 
-    private static void reverse(int[] nums,int start, int end){
-        int temp=0;
-        while(start<end){
-        temp=nums[start];
-        nums[start]=nums[end];
-        nums[end]=temp;
-        start++;
-        end--;    
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4,5,6,7};
+        rotate(nums1, 3);
+        System.out.println(Arrays.toString(nums1));
+
+        int[] nums2 = {-1,-100,3,99};
+        rotate(nums2, 2);
+        System.out.println(Arrays.toString(nums2));
     }
 }
